a0edde92e82e59a02610b19d22609771bdac9a48
BANAYEEM, HASSAN (EXTERN FDTECH) <hassan.banayeem@fdtech.de>
2022-05-13 23:30:43 +0200

    final refactored v1

--- AutoGitPush.py
+++ AutoGitPush.py
@@ -6,72 +6,69 @@ import datetime
 from credentials import GITHUB_TOKEN
 
 
-def push_file(fileName, repo_slug, branch, user, token):
+def push_file(fileName, repo_url, branch, user, token):
     '''
     Push file update to GitHub repo
-    
+
     // param fileName: the name of the file on the local branch
-    // param repo_slug: the github repo slug, i.e. "repository_url": "https://api.github.com/repos/{owner}/{repo}"
+    // param repo_url: the github repo slug, i.e. "https://api.github.com/repos/{owner}/{repo}"
     // param branch: the name of the branch to push the file to
     // param user: github username
     // param token: github user token
     // return None
     // raises Exception: if file with the specified name cannot be found in the repo
     '''
-    
+
     message = f"Automated commit/push created for the file {fileName} at {str(datetime.date.today())}"
-    path = "https://api.github.com/repos/%s/branches/%s" % (repo_slug, branch)
+    url = "https://api.github.com/repos/%s/branches/%s" % (repo_url, branch)
 
-    r = requests.get(path, auth=(user,token))
-
-    # Error Check Handling
+    # get the response of the repo_url/branch
+    r = requests.get(url, auth=(user, token))
     if not r.ok:
-        print("Error when retrieving branch info from %s" % path)
+        print("Error when retrieving branch info from %s" % url)
         print("Reason: %s [%d]" % (r.text, r.status_code))
-        
-    rjson = r.json() # Getting json keys
-    
+    rjson = r.json()
     treeurl = rjson['commit']['commit']['tree']['url']
-
-    # print(treeurl)
-    r2 = requests.get(treeurl, auth=(user,token))
+    # get the response of the file insidde the repo_url/branch/tree
+    r2 = requests.get(treeurl, auth=(user, token))
     if not r2.ok:
         print("Error when retrieving commit tree from %s" % treeurl)
         print("Reason: %s [%d]" % (r2.text, r2.status_code))
-
-    
     r2json = r2.json()
-    sha = None
 
+    # Set sha as None by default to avoid error if file is not found
+    sha = None
     for file in r2json['tree']:
-        # Found file, get the sha code
+        # If file is found in the repo, set the sha to the file's sha
         if file['path'] == fileName:
             sha = file['sha']
-
-    # if sha is None after the for loop, we did not find the file name!
-    if sha is None:
-        print ("\nThe file " + fileName + " is not in repos 'tree'. \nLet's create a new one .. \n", end=",\n 1 \n 2 \n 3 \n")
 
     with open(fileName, 'rb') as data:
         byte_content = data.read()
         content = base64.b64encode(byte_content).decode("ascii")
 
-    # gathered all the data, now let's push
+    # Gather all data to commit and push
     inputdata = {}
     inputdata["branch"] = branch
     inputdata["message"] = message
     inputdata["content"] = content
-
     if sha:
         inputdata["sha"] = str(sha)
+    else:
+        print("The files: %s, are not found in repos tree %s \nLet's commit the new file and push it.. \n" %
+              (fileName, repo_url))
 
-    updateURL = f"https://api.github.com/repos/{repo_slug}/contents/{fileName}"  
+    # Access the url content of the repo where the file is to be committed and pushed
+    updateURL = f"https://api.github.com/repos/{repo_url}/contents/{fileName}"
+
+    #  Finally, commit and push the file
     try:
-        rPut = requests.put(updateURL, auth=(user,token), data = json.dumps(inputdata))
+        rPut = requests.put(updateURL, auth=(user, token),
+                            data=json.dumps(inputdata))
+        print("Push successful!\n")
         if not rPut.ok:
             print("Error when pushing to %s" % updateURL)
             print("Reason: %s [%d]" % (rPut.text, rPut.status_code))
-        print("Done!!\n")
 
     except requests.exceptions.RequestException as e:
         print('Something went wrong! I will print all the information that is available so you can figure out what happend!')
@@ -86,6 +83,6 @@ if __name__ == '__main__':
     repositoryName = "hassanFDtech/AutoGitPush"
     branch = "main"
     username = "hassanFDtech"
-    token = GITHUB_TOKEN # from credentials.py
+    token = GITHUB_TOKEN  # from credentials.py
 
-    push_file(fileName,repositoryName,branch,user=username,token=token)
+    push_file(fileName, repositoryName, branch, username, token)
